5f673dd8f5ac272e7fe9ef4fb9844cd05c00c35d
Abraham <abrahamgil2@gmail.com>
2020-04-25 04:45:42 -0500

    still trying to figure out connection to postgresDB issues

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://admin:password@localhost/height_collector'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://abraham:password@localhost/collector_height'
 db=SQLAlchemy(app)
 
 class Data(db.Model):
