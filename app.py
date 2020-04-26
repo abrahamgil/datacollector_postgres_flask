fc387ab41d149ecd923a5c8362ad1795cf779804
Abraham <abrahamgil2@gmail.com>
2020-04-26 12:14:32 -0500

    fixed port error for postgres but still can't create table from script :/

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://abraham:password@localhost/collector_height'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:password@localhost/collector_height'
 db=SQLAlchemy(app)
 
 class Data(db.Model):
